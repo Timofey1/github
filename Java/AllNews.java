43b044ca54276a37ca5a8cff07b2ad372704b888
Timofey <timtwin2@mail.ru>
2017-12-21 16:19:38 +0300

    java

--- Java/AllNews.java
+++ Java/AllNews.java
@@ -1,14 +1,75 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
+
 public class AllNews {
+
     /**
-     * get list of sourses of user
-     * @param userId user number
-     * @return user's sourses
-     * @author andtun
+     * get response
+     * @param login user login
+     * @return String
+     * @throws IOException
      */
-    public static String[] getSbs(Integer userId) {}
+        public static String getresp(String login) throws IOException { //arg=String login
+        String url = "http://news.jkdev.ru/" + login + "/uinfo";
+        String doc = Jsoup.connect(url).get().text();
+        String response = doc.toString();
+        return response;
+    }
+
+    /**
+     * get vk subscriptions
+     * @param login user login
+     * @return ArrayList<String>
+     * @throws JSONException
+     */
+    public static ArrayList<String> vkSubs(String login) throws JSONException, IOException {
+        String resp = getresp(login);
+        JSONObject obj = new JSONObject(resp);
+        JSONArray vk_subs = obj.getJSONObject("subscriptions").getJSONArray("vk");
+        ArrayList<String> vkSubsArr = new ArrayList<String>((Collection<? extends String>) vk_subs);
+        return vkSubsArr;
+    }
+
+    /**
+     * get inst subscriptions
+     * @param login user login
+     * @return ArrayList<String>
+     * @throws JSONException
+     */
+    public static ArrayList<String> instagramSubs(String login) throws JSONException, IOException {
+        String resp = getresp(login);
+        JSONObject obj = new JSONObject(resp);
+        JSONArray instagram_subs = obj.getJSONObject("subscriptions").getJSONArray("istagram");
+        ArrayList<String> instagramSubsArr = new ArrayList<String>((Collection<? extends String>) instagram_subs);
+        return instagramSubsArr;
+    }
+
+    /**
+     * get all subscriptions
+     * @param login
+     * @return ArrayList<String>
+     * @throws IOException
+     * @throws JSONException
+     */
+    public static ArrayList<String> getSubs(String login) throws IOException, JSONException {
+        ArrayList<String> subs = new ArrayList<>();
+        ArrayList<String> vksubs = vkSubs(login);
+        for (String sub: vksubs) {
+            subs.add(sub);
+        }
+        ArrayList<String> intagramsubs = vkSubs(login);
+        for (String sub: intagramsubs) {
+            subs.add(sub);
+        }
+        return subs;
+    }
 
     /**
      * swap two objects
@@ -52,15 +113,15 @@ public class AllNews {
     }
 
     /**
-     * выдает 10 последних новостей
-     * @param userId номер пользователя
-     * @return ретурн
+     * 10 last news
+     * @param login user login
+     * @return Post[]
      */
-    public static Post[] getLastNews(Integer userId) {
-        String[] subs = getSbs(userId);
+    public static Post[] getLastNews(String login) throws IOException, JSONException {
+        ArrayList<String> subs = getSubs(login);
         ArrayList<Post> postObjs = new ArrayList<Post>();
         for (String sub: subs) {
-            postObjs.add(PARSER);
+            postObjs.add(pars);
         }
         sortDate(postObjs);
         Post[] result = new Post[10];
@@ -71,15 +132,15 @@ public class AllNews {
     }
 
     /**
-     * выдает 10 лучших новостей
-     * @param userId номер пользователя
-     * @return ретурн
+     * 10 best news
+     * @param login user login
+     * @return Post[]
      */
-    public static Post[] getBestNews(Integer userId) {
-        String[] subs = getSbs(userId);
+    public static Post[] getBestNews(String login) throws IOException, JSONException {
+        ArrayList<String> subs = getSubs(login);
         ArrayList<Post> postObjs = new ArrayList<Post>();
         for (String sub: subs) {
-            postObjs.add(PARSER);
+            postObjs.add(pars);
         }
         sortRate(postObjs);
         Post[] result = new Post[10];
@@ -87,6 +148,52 @@ public class AllNews {
             result[j] = postObjs.get(j);
         }
         return result;
+    }
+
+    /**
+     * add Subscription
+     * @param login username
+     * @param socialNetwork type of SN like vk or instagram
+     * @param snurl url of sourse
+     * @return response String
+     * @throws IOException
+     */
+    public static String addSub(String login, String socialNetwork, String snurl) throws IOException {
+        String url = String.format("http://news.jkdev.ru/%s/addSubscription?source=%s&subscriptions=%s", login, socialNetwork, snurl);
+        String doc = Jsoup.connect(url).get().text();
+        String response = doc.toString();
+        return response;
+    }
+
+    /**
+     * delete subscription
+     * @param login username
+     * @param socialNetwork type of SN like vk or instagram
+     * @param snurl url of sourse
+     * @return response String
+     * @throws IOException
+     */
+    public static String delSub(String login, String socialNetwork, String snurl) throws IOException {
+        String url = String.format("http://news.jkdev.ru/%s/deleteSubscription?source=%s&subscriptions=%s", login, socialNetwork, snurl);
+        String doc = Jsoup.connect(url).get().text();
+        String response = doc.toString();
+        return response;
+    }
+
+    /**
+     * New user
+     * @param login user name
+     * @param email email (if he has one)
+     * @param telegram telegram acc (if he has one)
+     * @return response String
+     * @throws IOException
+     */
+    public static String register(String login, String email, String telegram) throws IOException {
+        String pwd = "qwerty";
+        String url = String.format("http://news.jkdev.ru/%s/register?pwd=%s&mail=%s&telegram=%s", login, pwd, email, telegram);
+        String doc = Jsoup.connect(url).get().text();
+        String response = doc.toString();
+        return response;
     }
 
 }
