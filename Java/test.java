44e6faf9dada518829655157e57c83caf28cc608
Timofey1 <timtwin2@mail.ru>
2019-10-28 14:15:14 +0300

    a

--- Java/test.java
+++
@@ -1,9 +0,0 @@
-import org.jsoup.Jsoup;
-import java.io.IOException;
-
-public class test {
-    public static void main(String[] args) throws IOException {
-        String r = AllNews.getresp("q");
-        System.out.println(r);
-    }
-}
